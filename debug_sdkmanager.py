2ee294a089355846e56ec30ae306a81fbae496f7
sunwc <sunwc@fun.tv>
2019-03-08 17:09:17 +0800

    优化

--- debug_sdkmanager.py
+++ debug_sdkmanager.py
@@ -9,6 +9,8 @@ import pandas as pd
 debug_task_top_dir = echarts_base_dir + '/out/debug_tasks/'
 debug_p2p_download_top_dir = echarts_base_dir + '/out/debug_p2p_download/'
 
+global save_macs
+
 
 def init():
     pd.set_option('display.max_columns', None)
@@ -16,19 +18,27 @@ def init():
     print(yesterday_str())
 
 
-def save(task, filename, mac):
+def save(task, filename, fcontent):
     try:
         target_file = debug_task_top_dir + task + '/' + filename
         os.makedirs(debug_task_top_dir + task, exist_ok=True)
-        fo = open(target_file, 'a+')
-        fo.write(mac + '\t' + yesterday_str() + '\n')
+        fo = open(target_file, mode='a+', buffering=409600)
+        fo.write(fcontent)
+        fo.flush()
         fo.close()
     except Exception as err:
         print('save1')
         print(task)
-        print(mac)
         print('save2')
         print(err)
+
+
+def save_batch(mac_dict, filename):
+    if (len(mac_dict) == 0):
+        return
+    keys = mac_dict.keys()
+    for key in keys:
+        save(key, filename, mac_dict[key])
 
 
 def duplicates_mac(target_file):
@@ -100,14 +110,27 @@ def txt_download_start():
         txt_download_start_sql = 'INSERT INTO debug_sdk_download_install (h_mode, sdk_ver, app, download_start, date) VALUES (\'' + row.h_mode + '\', \'' + str(row.app_ver) + '\', \'' + row.tver + '\', \'' + \
             str(row.count) + '\', \'' + yesterday_str() + '\');'
         mysql_execute(txt_download_start_sql)
+    download_start_save = 0
+    save_macs = {}
     for row in df_distinct_mac.itertuples():
         try:
-            save(row.tver, 'downloadStart', row.mac)
+            download_start_save += 1
+            current_line = (row.mac + '\t' + yesterday_str() + '\n')
+            if (row.tver in save_macs):
+                save_macs[row.tver] = (save_macs[row.tver] + current_line)
+            else:
+                save_macs[row.tver] = current_line
+            if (download_start_save % 10000 == 0):
+                save_batch(save_macs, 'downloadStart')
+                print('save downloadStart index:' + str(download_start_save))
+                save_macs.clear()
         except Exception as err:
             print('downloadStart tver:' + row.tver)
             print('downloadStart mac:' + row.mac)
             print(err)
             continue
+    save_batch(save_macs, 'downloadStart')
+    save_macs.clear()
     for row in df_from_group.itertuples():
         # print(row)
         duplicates_mac(debug_task_top_dir + row.tver + '/' + 'downloadStart')
@@ -143,14 +166,27 @@ def txt_add_task_sucess():
             add_task_secuss_sql = 'INSERT INTO debug_sdk_download_install (h_mode, sdk_ver, app, add_task_secuss, date) VALUES (\'' + row.h_mode + '\',\'' + str(row.app_ver) + '\',\'' + row.tver + '\',\'' + \
                 str(row.count) + '\',\'' + yesterday_str() + '\');'
         mysql_execute(add_task_secuss_sql)
+    add_task_sucess_save = 0
+    save_macs = {}
     for row in df_distinct_mac.itertuples():
         try:
-            save(row.tver, 'addTaskSucess', row.mac)
+            add_task_sucess_save += 1
+            current_line = (row.mac + '\t' + yesterday_str() + '\n')
+            if (row.tver in save_macs):
+                save_macs[row.tver] = (save_macs[row.tver] + current_line)
+            else:
+                save_macs[row.tver] = current_line
+            if (add_task_sucess_save % 10000 == 0):
+                save_batch(save_macs, 'addTaskSucess')
+                print('save addTaskSucess index:' + str(add_task_sucess_save))
+                save_macs.clear()
         except Exception as err:
             print('addTaskSucess tver:' + row.tver)
             print('addTaskSucess mac:' + row.mac)
             print(err)
             continue
+    save_batch(save_macs, 'addTaskSucess')
+    save_macs.clear()
     for row in df_from_group.itertuples():
         # print(row)
         duplicates_mac(debug_task_top_dir + row.tver + '/' + 'addTaskSucess')
@@ -188,14 +224,27 @@ def txt_download_sucess():
             txt_download_sucess_sql = 'INSERT INTO debug_sdk_download_install (h_mode, sdk_ver, app, download_sucess, date) VALUES (\'' + row.h_mode + '\',\'' + str(row.app_ver) + '\',\'' + row.tver + '\',\'' + \
                 str(row.count) + '\',\'' + yesterday_str() + '\');'
         mysql_execute(txt_download_sucess_sql)
+    download_sucess_save = 0
+    save_macs = {}
     for row in df_distinct_mac.itertuples():
         try:
-            save(row.tver, 'downloadSucess', row.mac)
+            download_sucess_save += 1
+            current_line = (row.mac + '\t' + yesterday_str() + '\n')
+            if (row.tver in save_macs):
+                save_macs[row.tver] = (save_macs[row.tver] + current_line)
+            else:
+                save_macs[row.tver] = current_line
+            if (download_sucess_save % 10000 == 0):
+                save_batch(save_macs, 'downloadSucess')
+                print('save downloadSucess index:' + str(download_sucess_save))
+                save_macs.clear()
         except Exception as err:
             print('downloadSucess tver:' + row.tver)
             print('downloadSucess mac:' + row.mac)
             print(err)
             continue
+    save_batch(save_macs, 'downloadSucess')
+    save_macs.clear()
     for row in df_from_group.itertuples():
         # print(row)
         duplicates_mac(debug_task_top_dir + row.tver + '/' + 'downloadSucess')
@@ -234,14 +283,27 @@ def txt_install_sucess():
             txt_install_sucess_sql = 'INSERT INTO debug_sdk_download_install (h_mode, sdk_ver, app, install_sucess, date) VALUES (\'' + row.h_mode + '\',\'' + str(row.app_ver) + '\',\'' + row.tver + '\',\'' + \
                 str(row.count) + '\',\'' + yesterday_str() + '\');'
         mysql_execute(txt_install_sucess_sql)
+    install_sucess_save = 0
+    save_macs = {}
     for row in df_distinct_mac.itertuples():
         try:
-            save(row.tver, 'installSucess', row.mac)
+            install_sucess_save += 1
+            current_line = (row.mac + '\t' + yesterday_str() + '\n')
+            if (row.tver in save_macs):
+                save_macs[row.tver] = (save_macs[row.tver] + current_line)
+            else:
+                save_macs[row.tver] = current_line
+            if (install_sucess_save % 10000 == 0):
+                save_batch(save_macs, 'installSucess')
+                print('save installSucess index:' + str(install_sucess_save))
+                save_macs.clear()
         except Exception as err:
             print('installSucess tver:' + row.tver)
             print('installSucess mac:' + row.mac)
             print(err)
             continue
+    save_batch(save_macs, 'installSucess')
+    save_macs.clear()
     for row in df_from_group.itertuples():
         # print(row)
         duplicates_mac(debug_task_top_dir + row.tver + '/' + 'installSucess')
@@ -250,107 +312,5 @@ def txt_install_sucess():
 ############################################################P2P#######################################################################
 
 
-def p2p_save(task, filename, mac, ok):
-    try:
-        target_file = debug_p2p_download_top_dir + task + '/' + filename
-        os.makedirs(debug_p2p_download_top_dir + task, exist_ok=True)
-        fo = open(target_file, 'a+')
-        fo.write(mac + '\t' + ok + '\t' + yesterday_str() + '\n')
-        fo.close()
-    except Exception as err:
-        print('save1')
-        print(task)
-        print(mac)
-        print('save2')
-        print(err)
-
-
-def p2p_duplicates_mac(target_file):
-    data = pd.read_table(target_file, names=['mac', 'ok', 'date'])
-    df_total = pd.DataFrame(data)
-    df_total.drop_duplicates(subset=['mac', 'ok'], keep='last', inplace=True)
-    df_total.to_csv(target_file, sep='\t', header=False, index=False)
-
-
-def p2p_del_old_mac(df, filename):
-    print('p2p_del_old_mac start df:' + str(len(df)))
-    global df_source
-    df_source = pd.DataFrame().append(df)
-
-    df_tvers = df.groupby(['tver']).size().reset_index()
-    # print(df_tvers)
-    tvers = []
-    for row in df_tvers.itertuples():
-        tvers.append(row.tver)
-
-    for tver in tvers:
-        #print(tver + '------df_source len:' + str(len(df_source)))
-        old_macs = [[]]
-        target_file = debug_p2p_download_top_dir + tver + '/' + filename
-        print(target_file)
-        if (os.path.exists(target_file)):
-            data = pd.read_table(target_file, names=['mac', 'ok', 'date'])
-            df_mac = pd.DataFrame(data)
-            for mac_row in df_mac.itertuples():
-                p2p_list = []
-                p2p_list.append(mac_row.mac)
-                p2p_list.append(mac_row.ok)
-                old_macs.append(p2p_list)
-            print(old_macs)
-            df_source = df_source[~((df_source['tver'] == tver) & (
-                old_macs.count([df_source.mac, df_source.ok])))]
-        else:
-            pass
-    print('p2p_del_old_mac end df:' + str(len(df_source)))
-    return df_source
-
-
-def txt_p2p_error():
-    print('txt_p2p_error')
-    mysql_create_table('debug_sdk_p2p_error',
-                       debug_sdk_p2p_error_create_sql)
-    if (mysql_inserted('debug_sdk_p2p_error') > 0):
-        return
-    data = pd.read_table(filename_pre + yesterday_str() +
-                         filename_mid + 'out3.txt', names=tv_download_apk_start_columns)
-    df = pd.DataFrame(data)
-    df.fillna(0, inplace=True)
-    df.drop_duplicates(subset=['mac', 'tver', 'ok'], keep='last', inplace=True)
-    df['app_ver'] = df['app_ver'].astype('int')
-    df['ok'] = df['ok'].astype('int')
-    df_distinct_mac = p2p_del_old_mac(df, 'p2p_download_ok')
-    if (len(df_distinct_mac) == 0):
-        return
-
-    groups = df.groupby(['h_mode', 'app_ver', 'tver', 'ok'])
-    df_from_group = groups.size().reset_index(name='count')
-    # print(df_from_group)
-    for row in df_from_group.itertuples():
-        print(row)
-        txt_p2p_error_sql = 'INSERT INTO debug_sdk_p2p_error (h_mode, sdk_ver, app, error_code, count, date) VALUES (\'' + row.h_mode + '\',\'' + str(
-            row.app_ver) + '\',\'' + row.tver + '\',\'' + str(row.ok) + '\',\'' + str(row.count) + '\',\'' + yesterday_str() + '\');'
-        mysql_execute(txt_p2p_error_sql)
-    for row in df_distinct_mac.itertuples():
-        try:
-            p2p_save(row.tver, 'p2p_download_ok', row.mac, str(row.ok))
-        except Exception as err:
-            print('p2p_download_ok tver:' + row.tver)
-            print('p2p_download_ok mac:' + row.mac)
-            print(err)
-            continue
-    for row in df_from_group.itertuples():
-        # print(row)
-        p2p_duplicates_mac(debug_p2p_download_top_dir +
-                           row.tver + '/' + 'p2p_download_ok')
-
-
 init()
 txt_download_install()
-# txt_p2p_error()
-'''
-sss = '123'
-bbb = 2
-aList = []  # [123, 'Google', 'Runoob', 'Taobao', ['123', 2]]
-aList.append(['123', 2])
-print("123 元素个数 : ", 1 & aList.count([sss, bbb]))
-'''
--- fun/fun_file_util.py
+++ fun/fun_file_util.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 #!/usr/bin/python
 
 from fun_constant import *
@@ -7,6 +7,7 @@ import os
 import pandas as pd
 import time
 
+
 def fun_format_file(path):
     files = os.listdir(path)
     for i in range(0, len(files)):
@@ -14,7 +15,7 @@ def fun_format_file(path):
         if (os.path.isfile(sourcefile)):
             print(sourcefile)
             insert_column(sourcefile)
-            #print_df(sourcefile)
+            # print_df(sourcefile)
             '''
             print(sourcefile)
             content = ''
@@ -33,15 +34,38 @@ def fun_format_file(path):
         else:
             fun_format_file(sourcefile)
 
+
 def insert_column(path):
     try:
-        data = pd.read_table(path, names = ['mac'])
+        data = pd.read_table(path, names=['mac'])
         df = pd.DataFrame(data)
         df = df.reindex(columns=['mac', 'date'], fill_value='20190124')
-        df.to_csv(path, sep='\t', header = False, index = False)
+        df.to_csv(path, sep='\t', header=False, index=False)
     except:
         print('column already exist')
 
+
+def update_tasks_file_by_date(path, date):
+    files = os.listdir(path)
+    for i in range(0, len(files)):
+        sourcefile = os.path.join(path, files[i])
+        if (os.path.isfile(sourcefile)):
+            print(sourcefile)
+            del_mac_by_date(sourcefile, date)
+            pass
+        else:
+            update_tasks_file_by_date(sourcefile, date)
+
+
+def del_mac_by_date(target_file, date):
+    data = pd.read_table(target_file, names=['mac', 'date'])
+    df_total = pd.DataFrame(data)
+    df_valid = df_total[df_total['date'] <= date]
+    df_valid.to_csv(target_file, sep='\t', header=False, index=False)
+
+
 if __name__ == '__main__':
     pass
-    #fun_format_file('/home/sunwc/romshare/SDK_ECharts/out/tasks')
+    # fun_format_file('/home/sunwc/romshare/SDK_ECharts/out/tasks')
+    # update_tasks_file_by_date(
+    #    '/home/sunwc/romshare/SDK_ECharts/out/debug_tasks', 20190301)
